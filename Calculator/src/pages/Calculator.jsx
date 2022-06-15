f919281cc838178e0516c2a2fcb4667b8490c78e
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-15 13:58:29 -0300

    Init calculator project

---
+++ Calculator/src/pages/Calculator.jsx
@@ -0,0 +1,18 @@
+import { StyleSheet, View, Text } from 'react-native'
+
+export default function Calculator() {
+  return(
+    <View style={styles.calculator}> 
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  calculator: {
+    backgroundColor: '#262429',
+    width: '85%',
+    height: 500,
+    borderRadius: 10
+  },
+});
