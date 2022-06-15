cb9fba60d7c701e1679dccab066bbd8a4437645d
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-15 16:00:09 -0300

    Adding onclick in buttons

--- Calculator/src/components/Button.jsx
+++ Calculator/src/components/Button.jsx
@@ -1,8 +1,32 @@
 import { StyleSheet, Text, Dimensions, TouchableHighlight } from 'react-native' 
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import MaterialCommunityIcons
+from 'react-native-vector-icons/MaterialCommunityIcons'
 
 const device_width = Dimensions.get('window').width
 
 export default function Button({label, onClick, bgColor = '#383941', color = '#e6e6e6', big = false}) {
+
+  function setOperatorIcon() {
+    switch (label) {
+      case '/': 
+        return <FontAwesome5 name="divide" size={25} color="#e6e6e6" />
+      case '*': 
+        return <Ionicons name="close" size={35} color="#e6e6e6" />
+      case '-': 
+        return <FontAwesome name="minus" size={25} color="#e6e6e6" />
+      case '+': 
+        return <FontAwesome name="plus" size={25} color="#e6e6e6" />
+      case '=': 
+        return <FontAwesome5 name="equals" size={25} color="#e6e6e6" />
+      case '+/-': 
+        return <MaterialCommunityIcons name="plus-minus-variant" size={30} color="#333" />
+      default: 
+        return label
+    }
+  }
 
   return(
     <TouchableHighlight 
@@ -10,9 +34,9 @@ export default function Button({label, onClick, bgColor = '#383941', color = '#e
         backgroundColor: bgColor,
         width: big ? device_width / 2.5 : device_width / 6,
       }]}
-      onPress={onClick}
+      onPress={() => onClick(label)}
     >
-      <Text style={[styles.text, { color: color }]}>{label}</Text>
+      <Text style={[styles.text, { color: color }]}>{setOperatorIcon()}</Text>
     </TouchableHighlight>
   )
 }    
--- Calculator/src/pages/Calculator.jsx
+++ Calculator/src/pages/Calculator.jsx
@@ -1,36 +1,65 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { StyleSheet, View } from 'react-native'
 
-import Display from '../components/Display';
+import Display from '../components/Display'
 import Button from '../components/Button'
 
 export default function Calculator() {
 
-  const [display_label, setDisplayLabel] = useState(120)
+  const [display_label, setDisplayLabel] = useState(['0'])
+  const [first_number, setFirstNumber] = useState(0)
+  const [second_number, setSecondNumber] = useState(0)
+  const [operation, setOperator] = useState()
+  console.log(display_label)
+
+  // useEffect(() => {
+
+  // }, [operation])
+
+  function addDigit(digit){
+    // if (display_label[0] == '0' && digit !== ',' && !display_label.includes(',')) {
+    //   setDisplayLabel([])
+    // }
+    // if (digit == ',' && display_label.includes(',')) return 
+
+    // setDisplayLabel(state => [...state, String(digit)])
+  }
+
+  function clearMemory(){
+    setDisplayLabel('0')
+  }
+
+  function setOperation(operator){
+    setOperator(operator)
+  }
+
+  function solution(){
+    // let total = 
+  }
 
   return(
     <View style={styles.calculator}> 
       <Display display_label={display_label}/>
       <View style={styles.buttonsArea}>
-        <Button label='AC' bgColor='#aaa' color='#333'/>
-        <Button label='+/-' bgColor='#aaa' color='#333'/>
-        <Button label='%' bgColor='#aaa' color='#333'/>
-        <Button label='/' bgColor='#f6971a'/>
-        <Button label='7'/>
-        <Button label='8'/>
-        <Button label='9'/>
-        <Button label='X' bgColor='#f6971a'/>
-        <Button label='4'/>
-        <Button label='5'/>
-        <Button label='6'/>
-        <Button label='-' bgColor='#f6971a'/>
-        <Button label='1'/>
-        <Button label='2'/>
-        <Button label='3'/>
-        <Button label='+' bgColor='#f6971a'/>
-        <Button label=','/>
-        <Button label='0' big />
-        <Button label='=' bgColor='#f6971a'/>
+        <Button label='AC' bgColor='#aaa' color='#333' onClick={clearMemory}/>
+        <Button label='+/-' bgColor='#aaa'/>
+        <Button label='%' bgColor='#aaa' color='#333' onClick={setOperation}/>
+        <Button label='/' bgColor='#f6971a' onClick={setOperation}/>
+        <Button label='7' onClick={addDigit}/>
+        <Button label='8' onClick={addDigit}/>
+        <Button label='9' onClick={addDigit}/>
+        <Button label='*' bgColor='#f6971a' onClick={setOperation}/>
+        <Button label='4' onClick={addDigit}/>
+        <Button label='5' onClick={addDigit}/>
+        <Button label='6' onClick={addDigit}/>
+        <Button label='-' bgColor='#f6971a' onClick={setOperation}/>
+        <Button label='1' onClick={addDigit}/>
+        <Button label='2' onClick={addDigit}/>
+        <Button label='3' onClick={addDigit}/>
+        <Button label='+' bgColor='#f6971a' onClick={setOperation}/>
+        <Button label=',' onClick={addDigit}/>
+        <Button label='0' big onClick={addDigit}/>
+        <Button label='=' bgColor='#f6971a' onClick={solution}/>
       </View>
     </View>
   )
