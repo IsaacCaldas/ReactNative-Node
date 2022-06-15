95562eb8cb940200f73af2ceebd2d8c75719f3fd
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-15 10:27:34 -0300

    Flexbox: flex grow

--- Flexbox/App.js
+++ Flexbox/App.js
@@ -3,7 +3,8 @@ import { StyleSheet, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box/>
+      <BoxGrow/>
+      {/* <Box/> */}
     </View>
   );
 }
@@ -11,11 +12,11 @@ export default function App() {
 const Box = () => {
   return (
     <View style={styles.box}>
-      <Square side={20}/>
+      {/* <Square side={20}/>
       <Square color='#a3A' side={30}/>
       <Square color='#ddde23' side={60}/>
       <Square color='#a3f2ba' side={40}/>
-      <Square color='#4a33bb' side={10}/>
+      <Square color='#4a33bb' side={10}/> */}
     </View>
   )
 }
@@ -29,6 +30,16 @@ const Square = ({color, side}) => {
   )
 }
 
+const BoxGrow = () => {
+  return (
+    <View style={styles.box}>
+      <View style={styles.v0}/>
+      <View style={styles.v1}/>
+      <View style={styles.v2}/>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#333',
@@ -38,11 +49,20 @@ const styles = StyleSheet.create({
     padding: 20
   },
   box: {
-    flexDirection: 'row-reverse',
-    justifyContent: 'space-evenly',
-    alignItems: 'stretch',
-    width: '100%',
-    height: 400,
+    flex: 1,
+    width: 100,
     backgroundColor: '#111'
+  },
+  v0: {
+    backgroundColor: '#f32b3a',
+    height: 150,
+  },
+  v1: {
+    backgroundColor: '#3aab32',
+    flexGrow: 3,
+  },
+  v2: {
+    backgroundColor: '#3faffc',
+    flexGrow: 5,
   }
 });
