65fd1355f95d2710fa8ea8f0a086d1659b5a0a6b
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-23 09:18:05 -0300

    Set params in index

--- Minefield/App.js
+++ Minefield/App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import params from './src/utils/params'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>mine<Text style={styles.titleSub}>Field</Text></Text> 
+      <Text style={styles.gridSize}>
+        Grid size: {params.getRowsAmount()}x{params.getColumnsAmount()} 
+      </Text> 
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 38,
+    color: '#ebebeb',
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  titleSub: {
+    fontSize: 30,
+    color: "#a32a22"
+  },
+  gridSize: {
+    fontSize: 20,
+    color: "#ebebeb"
+  }
 });
--- Minefield/src/utils/params.js
+++ Minefield/src/utils/params.js
@@ -7,12 +7,14 @@ const params = {
   headerRatio: 0.15, 
   difficultLevel: 0.1,
   getColumnsAmount() {
-    const width = Dimensions.get('Window').width
+    const width = Dimensions.get('window').width
     return Math.floor(width / this.blockSize)
   },
   getRowsAmount() {
-    const totalHeight = Dimensions.get('Window').height
+    const totalHeight = Dimensions.get('window').height
     const boardHeight = totalHeight * (1 - this.headerRatio)
     return Math.floor(boardHeight / this.blockSize)
   }
 }
+
+export default params
