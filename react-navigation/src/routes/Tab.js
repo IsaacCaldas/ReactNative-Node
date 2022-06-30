d382a3f9861ca26493d3c2f3723e27557cdecf8e
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-06-30 16:15:13 -0300

    Tab icons

--- react-navigation/src/routes/Tab.js
+++ react-navigation/src/routes/Tab.js
@@ -4,19 +4,51 @@ import Home from '../pages/Tab/Home'
 import About from '../pages/Tab/About'
 import Contact from '../pages/Tab/Contact'
 
+import IonIcons from 'react-native-vector-icons/Ionicons'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
 const Tab = createBottomTabNavigator()
 
 export default function TabNav() {
   return (
     <Tab.Navigator
+      screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarIcon: ({focused, color, size}) => {
+            let icon_name
+
+            switch (route.name) {
+              case 'Home':
+                icon_name = focused ? 'ios-home' : 'home-outline'
+                break;
+              case 'About':
+                  icon_name = focused ? 'information-circle' : 'information-circle-outline'
+                  break;
+              case 'Contact':
+                icon_name = focused ? 'contacts' : 'contacts-outline'
+                break;
+            }
+
+            return (
+              <>
+                { route.name == 'Contact' ?
+                    <MaterialCommunityIcons name={icon_name} size={size} color={color}/>
+                    : 
+                    <IonIcons name={icon_name} size={size} color={color}/>
+                }
+              </>
+            )
+          }
+        })
+      }
       tabBarOptions={{
         activeTintColor: '#eee',
         inactiveTintColor: '#aaa',
         activeBackgroundColor: '#111',
         inactiveBackgroundColor: '#111',
+        showLabel: false,
         labelStyle: { fontSize: 16 }
       }}
-      screenOptions={{ headerShown: false }}
       initialRouteName="Home"
     >
       <Tab.Screen name='About' component={About}/>
