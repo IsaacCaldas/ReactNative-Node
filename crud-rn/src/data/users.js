9bfbc51bf32ddc06febe8590aeaa69008fb04e7b
Isaac Tavares Caldas Gonçalves <isaaccaldasgoncalves@gmail.com>
2022-07-01 10:30:27 -0300

    User data seed

---
+++ crud-rn/src/data/users.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 0,
+    name: 'Javier Leone',
+    email: 'javier.leone@gmail.com',
+    img: 'https://cdn.pixabay.com/photo/2016/03/31/19/57/avatar-1295404_960_720.png'
+  },
+  {
+    id: 1,
+    name: 'Jonathan Silveira',
+    email: 'jonathan.silveira@gmail.com',
+    img: 'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png'
+  },
+  {
+    id: 2,
+    name: 'Gabriela Freitas',
+    email: 'gabriela.freitas@gmail.com',
+    img: 'https://cdn.pixabay.com/photo/2022/06/23/00/09/avatar-7278926_960_720.png'
+  },
+  {
+    id: 3,
+    name: 'Sílvio Dantas',
+    email: 'silvio.dantas@gmail.com',
+    img: 'https://cdn.pixabay.com/photo/2016/12/13/16/17/dancer-1904467_960_720.png'
+  },
+  {
+    id: 4,
+    name: 'Michelle Tereza Pinheiro',
+    email: 'michelle.pinheiro@gmail.com',
+    img: 'https://cdn.pixabay.com/photo/2017/01/31/19/07/avatar-2026510_960_720.png'
+  },
+]
